49fbac7f6ec0d31100fc11bfb19724c9d3e35fd8
sadanandkrishnaji <sadanand.dodawadakar@mavenir.com>
2023-01-16 12:56:45 +0530

    Committing basic code

---
+++ src/BasicPrograms/Training/Day1/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func main() {
+	var a int = 10
+
+	log.Println("Value of a:", a)
+	fmt.Println("Hello world...")
+}
--- src/BasicPrograms/main.go
+++ src/BasicPrograms/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
+
+func init() {
+	log.SetPrefix("[MAIN]")
+	log.SetFlags(log.Flags())
+}
 
 func main() {
+	var a, b = 10, false
+	var x int
+	x = 0
+	var y string
+	y = ""
+	fmt.Println(x, y)
+	fmt.Printf("Tye of a is %T and type of b is %T", a, b)
 	fmt.Println("Welcome To Go Lang Basic Programs")
+	testYourCode()
+}
+
+func testYourCode() {
+	a := 10
+	fmt.Println("Value of a is ", a)
+	log.Fatal("What is this...?")
 }
