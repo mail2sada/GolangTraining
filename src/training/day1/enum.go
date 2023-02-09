5cc434c0cb5ce048a17c118a8f6d5dfc6b22b398
sadanandkrishnaji <sadanand.dodawadakar@mavenir.com>
2023-02-09 12:29:17 +0530

    Create enum.go

---
+++ src/training/day1/enum.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+type Direction int
+
+const (
+	North Direction = iota
+	South
+	East
+	West
+)
+
+func (d Direction) ToString()string{
+	res := ""
+	switch d {
+		case North:
+			res = "North"
+		case South:
+			res = "South"
+		case East:
+			res = "East"
+		case West:
+			res = "West"
+	}
+	return res
+}
+
+func main() {
+
+	fmt.Printf("Demo: Simulationing enum")
+
+	d := North
+
+	var direct Direction = South
+
+	const fixedDirect Direction = East
+
+	const oppFixedDirect Direction = West
+
+	fmt.Printf("%s\n%s\n%s\n%s\n", d.ToString(), direct.ToString(), fixedDirect.ToString(), oppFixedDirect.ToString)
+}
